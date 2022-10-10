aa60eca5636d9164b3892fa36620055b741dad84
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-10-09 17:26:43 -0700

    update contract pages

--- pages/contracts/[id].tsx
+++ pages/contracts/[id].tsx
@@ -1,13 +1,21 @@
 import type { NextPage } from 'next';
 import { useRouter } from 'next/router';
 
+import LoginForm from '@/components/LoginForm';
 
 const Contract : NextPage = () => {
   const router = useRouter();
   const { id: contractId } = router.query;
 
   return (
-    <h1>Contract : { contractId }</h1>
+    <div>
+      <h1>Contract : { contractId }</h1>
+
+      <div className="w-full">
+        hello
+      </div>
+
+    </div>
   );
 }
 
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,8 +1,18 @@
 import type { NextPage } from 'next';
+import Link from 'next/link';
+import LoginForm from '@/components/LoginForm';
 
 const Login : NextPage = () => {
   return (
-    <h1>login page</h1>
+    <div>
+      <h1>Login</h1>
+      <LoginForm />
+
+      <div className="w-full mx-auto text-center">
+        <p>If you don't have an account, request one <span className="text-blue-800 italic underline"><Link href="/signup">here</Link></span></p>
+      </div>
+
+    </div>
   )
 }
 
