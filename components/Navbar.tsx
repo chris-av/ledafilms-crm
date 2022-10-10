a04c70cdd2859b7be491052523411658f6b64fc1
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-10-09 22:14:41 -0700

    make logo clickable and remove highlight on buttons

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,26 +6,30 @@ const Navbar: FC = () => {
     <div className="h-10 p-10 bg-slate-900 flex justify-between items-center text-xl text-white">
 
       <div className="flex justify-center cursor-pointer items-center w-10 select-none">
-        <img className="h-10" src="/ledafilms-icon-light.png" />
+        <Link href="/">
+          <a>
+            <img className="h-10" src="/ledafilms-icon-light.png" />
+          </a>
+        </Link>
       </div>
 
       <ul className="flex justify-between items-center text-xl">
-        <li className="mx-8">
+        <li className="mx-8 select-none">
           <Link href="/contracts">
             <a>Contracts</a>
           </Link>
         </li>
-        <li className="mx-8">
+        <li className="mx-8 select-none">
           <Link href="/">
             <a>About</a>
           </Link>
         </li>
-        <li className="mx-8">
+        <li className="mx-8 select-none">
           <Link href="/contact">
             <a>Contact</a>
           </Link>
         </li>
-        <li className="mx-8">
+        <li className="mx-8 select-none">
           <Link href="/login">
             <a>Login</a>
           </Link>
