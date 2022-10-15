73e1d480c0c726807df6998a14483fbc5ffbf604
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-10-14 20:44:50 -0700

    create new pages that matches routes

---
+++ pages/contracts/add.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+
+
+const AddContract : NextPage = () => {
+  return (
+    <h1>Add Contract</h1>
+  );
+}
+
+
+export default AddContract;
+
---
+++ pages/titles/add.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next';
+
+
+const AddTitle: NextPage = () => {
+  return (
+    <h1>Add Title</h1>
+  );
+}
+
+
+export default AddTitle;
+
