cc41bc9436409fd8229cfb138ca3dccb8d90fc49
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-10-17 21:53:45 -0700

    render mock list of contracts

--- pages/contracts/index.tsx
+++ pages/contracts/index.tsx
@@ -1,9 +1,20 @@
 import type { NextPage } from 'next';
+import { contractsArr } from '@/data/contract.prototype.data';
+import Link from 'next/link';
 
 
 const Contracts : NextPage = () => {
   return (
-    <h1>Contracts</h1>
+    <div>
+      <h1>Contracts</h1>
+      <div className="flex flex-col justify-center items-center">
+        { contractsArr.map(contract => (
+          <Link href={`/contracts/${contract.contractNumber.replace("#", "")}`}>
+            <a>{contract.contractNumber}</a>
+          </Link>
+        )) }
+      </div>
+    </div>
   );
 }
 
