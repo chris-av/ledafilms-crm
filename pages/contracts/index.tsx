25cd422effaac2d04ca4de45df2710d1bf051e1d
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-11-07 23:05:47 -0800

    use server side props, query from backend

--- pages/contracts/index.tsx
+++ pages/contracts/index.tsx
@@ -1,21 +1,50 @@
 import type { NextPage } from 'next';
-import { contractsArr } from '@/data/contract.prototype.data';
 import Link from 'next/link';
 
 
-const Contracts : NextPage = () => {
+// TODO: replace this with proper types interface
+// right now, what is in the database is not really aligned with the page
+
+
+interface ContractProps {
+  contractType: string,
+  contractNumber: string,
+  licensor: string,
+  licensee: string,
+  dealStatus: string,
+  status: string,
+  dealType: string,
+  effectiveDate: string,
+  mg: string,
+  currency: string,
+  additonalTerms: string,
+  notes: string,
+};
+
+export function Contracts({ contracts } : { contracts: ContractProps[] }) {
+  console.log(contracts);
   return (
     <div>
       <h1>Contracts</h1>
       <div className="flex flex-col justify-center items-center">
-        { contractsArr.map(contract => (
-          <Link href={`/contracts/${contract.contractNumber}`}>
+        { contracts.map((contract, i) => (
+          <Link key={i} href={`/contracts/${contract.contractNumber}`}>
             <a>{contract.contractNumber}</a>
           </Link>
         )) }
       </div>
     </div>
   );
+}
+
+
+
+export async function getServerSideProps() {
+  const result = await fetch("https://localhost:3000/api/contracts");
+  const contracts = await result.json();
+  return {
+    props: { contracts }
+  }
 }
 
 
