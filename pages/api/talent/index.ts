d9e1777788b7a5be54aa6f50f718014596ebffa2
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-11-07 23:05:23 -0800

    api returns data from database, typed

--- pages/api/talent/index.ts
+++ pages/api/talent/index.ts
@@ -1,4 +1,5 @@
 import pool from '@/utils/pool';
+import { ITalent } from '@/utils/interfaces';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
@@ -8,12 +9,13 @@ type Data = {
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<{ data: Data[] }>
+  res: NextApiResponse<{ data: ITalent[] }>
 ) {
 
   try {
-    const result = await pool.query("SELECT * FROM talent LIMIT 3;");
-    res.status(200).json({ data: result.rows });
+    const result = await pool.query("SELECT * FROM talent LIMIT 100;");
+    const talent : ITalent[] = result.rows;
+    res.status(200).json({ data: talent });
   } catch (err: any) {
     res.status(400).json(err);
   }
