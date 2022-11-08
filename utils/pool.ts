1fe67c5694d88fc3fa36580c5f7ac0cf5472c70c
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-11-07 22:23:09 -0800

    shareable pool, entrypoint for database connection

---
+++ utils/pool.ts
@@ -0,0 +1,30 @@
+import { Pool } from 'pg';
+
+console.log({
+  host: process.env.PG_HOST,
+  user: process.env.PG_USER,
+  password: process.env.PG_PW,
+  database: process.env.PG_DB,
+})
+
+const pool = new Pool({
+  host: process.env.PG_HOST,
+  user: process.env.PG_USER,
+  password: process.env.PG_PW,
+  database: process.env.PG_DB,
+  port: 5439,
+  ssl: false
+});
+
+pool.connect(err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('connected to database!');
+    console.log('');
+  }
+});
+
+
+export default pool;
+
