2f9967336de3af1780eccd708d1ee93cedf90b1d
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-11-07 23:04:53 -0800

    contract route returns static data for now

--- pages/api/contracts/index.ts
+++ pages/api/contracts/index.ts
@@ -1,4 +1,5 @@
 import pool from '@/utils/pool';
+import { contractsArr } from '@/data/contract.prototype.data';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
@@ -12,8 +13,13 @@ export default async function handler(
 ) {
 
   try {
-    const result = await pool.query("SELECT * FROM contracts LIMIT 3;");
-    res.status(200).json({ data: result.rows });
+    // TODO: replace this with actual query result from database
+    // const result = await pool.query("SELECT * FROM contracts LIMIT 3;");
+    // res.status(200).json({ data: result.rows });
+
+
+    res.status(200).json({ data: contractsArr });
+
   } catch (err: any) {
     res.status(400).json(err);
   }
