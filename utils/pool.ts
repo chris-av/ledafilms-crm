35713afec4b28179406676ec0a4bd2f3d9c1b8a8
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-11-07 22:29:39 -0800

    add context in console log

--- utils/pool.ts
+++ utils/pool.ts
@@ -1,11 +1,15 @@
 import { Pool } from 'pg';
 
+
+console.log('environment variables : ');
 console.log({
   host: process.env.PG_HOST,
   user: process.env.PG_USER,
   password: process.env.PG_PW,
   database: process.env.PG_DB,
-})
+});
+console.log('');
+console.log('');
 
 const pool = new Pool({
   host: process.env.PG_HOST,
