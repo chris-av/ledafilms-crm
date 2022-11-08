87ee4bdd7bffdcfcdcd8cf285afbbe07c900d86b
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-11-07 22:22:49 -0800

    scaffold api backend routes

---
+++ pages/api/contracts/index.ts
@@ -0,0 +1,22 @@
+import pool from '@/utils/pool';
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+type Data = {
+  name: string
+}
+
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<{ data: Data[] }>
+) {
+
+  try {
+    const result = await pool.query("SELECT * FROM contracts LIMIT 3;");
+    res.status(200).json({ data: result.rows });
+  } catch (err: any) {
+    res.status(400).json(err);
+  }
+
+}
+
