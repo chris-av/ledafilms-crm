157a41e67453d351cf0332c5ac93bc5e29ab3dcf
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-11-16 20:33:01 -0800

    query database for single contract

---
+++ pages/api/contracts/[contract_id].ts
@@ -0,0 +1,18 @@
+import pool from '@/utils/pool';
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const { contract_id } = req.query;
+    const result = await pool.query("SELECT * FROM contracts where contract_id = $1", [ contract_id ]);
+    res.status(200).json({ data: result.rows });
+  } catch (err: any) {
+    res.status(400).json(err);
+  }
+}
+
+
