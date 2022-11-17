cd8d1731c224628df98c6fe06de2c5c45ff6aa1c
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-11-16 20:33:15 -0800

    update interface for contract

--- utils/interfaces/contract.ts
+++ utils/interfaces/contract.ts
@@ -1,6 +1,6 @@
 export enum ContractTypes {
-  Sales = "sales",
-  Acquisition = "acquisition"
+  Sales = "Sales",
+  Acquisition = "Acquisition"
 }
 
 export enum ContractDealStatuses {
@@ -24,15 +24,29 @@ export enum DealTypes {
 }
 
 export interface IContract {
-  territory: string,
-  right: string,
-  license_type: string,
-  start_date: string,
-  end_date: string,
-  start_confirmed: string,
-  end_confirmed: string,
-  unique_id: string,
+
+  contract_type: string,
+  licensor: string,
+  distributor: string,
+  status: string,
+  deal_status: string,
+  creation_date: string,
+  deal_type: string,
+  mg: number,
+  cur: string,
+  additional_terms: any,
+  notes: string,
   contract_id: string,
+
+  // territory: string,
+  // right: string,
+  // license_type: string,
+  // start_date: string,
+  // end_date: string,
+  // start_confirmed: string,
+  // end_confirmed: string,
+  // unique_id: string,
+
 }
 
 
