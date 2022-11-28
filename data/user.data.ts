3cec70ffc080ce6dc50c0f861c97fa84a43a9069
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-11-27 16:43:55 -0800

    create a test user for mockup

---
+++ data/user.data.ts
@@ -0,0 +1,8 @@
+export const testUser = {
+  name: "Christian Valenzuela",
+  username: "chris-user123",
+  role: "admin",
+  userCreated: new Date(),
+  userUpdated: new Date(),
+  avatar: null,
+}
