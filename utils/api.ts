18526c84c37b9e1c017cb43238c12fe3b53d150d
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-12-03 01:13:44 -0800

    define fetch helper

---
+++ utils/api.ts
@@ -0,0 +1,57 @@
+class LedaRequests {
+  url: string;
+  constructor(url: string) {
+    this.url = url
+  }
+
+  async getContract(id : string) {
+    try {
+      const response = await fetch(`${this.url}/contracts/${id}`);
+      const { data: contract } = await response.json();
+      return contract;
+    } catch (err) {
+      return err;
+    }
+  }
+
+  async getContracts() {
+    try {
+      const response = await fetch(`${this.url}/contracts/`);
+      const { data: contracts } = await response.json();
+      return contracts;
+    } catch (err) {
+      return err;
+    }
+  }
+
+  async getTalent(id: string) {
+    try {
+      const response = await fetch(`${this.url}/talent/${id}`);
+      const { data: talent } = await response.json();
+      return talent;
+    } catch (err) {
+      return err;
+    }
+  }
+
+  async getTalents() {
+    try {
+      const response = await fetch(`${this.url}/talent`);
+      const { data: talent } = await response.json();
+      return talent;
+    } catch (err) {
+      return err;
+    }
+  }
+
+}
+
+
+const url = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://wwww.myurl.com';
+const endpoint = url + '/api';
+
+const api = new LedaRequests(endpoint);
+
+
+export default api
+
