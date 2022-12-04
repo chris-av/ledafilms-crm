3ed1b161a4cea66e95f1786759eae466bace5f72
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-12-03 19:44:52 -0800

    update styling

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -13,19 +13,18 @@ interface ComponentProps {
 export default function Dropdown({ options, callback } : ComponentProps) {
   return (
     <div className="flex justify-center items-center bg-gray-200 w-full py-1 rounded">
-      <div className="cursor-pointer text-sm flex items-center">
-        (any)
-        <span className="ml-4">
-          <DownArrow
-            size={20}
-          />
-        </span>
+
+      <div className="relative">
+        <select className="block appearance-none w-full bg-gray-200 border border-gray-200 text-gray-700 px-4 rounded leading-tight focus:outline-none focus:border-gray-400">
+          { options.map(({ label, value }) => (
+            <option value={value}>{label}</option>
+          )) }
+        </select>
+        <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+          <DownArrow size={15} />
+        </div>
       </div>
-      <div className="hidden">
-        { options.map(({ label, value }) => (
-          <div key={value} onClick={() => callback(value)}>{ label }</div>
-        )) }
-      </div>
+
     </div>
   );
 }
