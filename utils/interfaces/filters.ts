c51316a79e955678eb680d4c7050c9f388620b14
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-12-05 21:24:15 -0800

    update types, document which tables

--- utils/interfaces/filters.ts
+++ utils/interfaces/filters.ts
@@ -8,10 +8,41 @@ type validOperators = '=' | '!=' | '~' |
   '>' | '>=';
 
 
+// maps to titles.titles
+export type validTitleTypeOpts = 'any' | 
+  'Film' | 'Series' | 'Mini-Series' | 
+  'Format' | 'Seasons' | 'Episodes';
+
+// maps to contracts.contract_type
+export type validContractTypeOpts = 'any' | 'Sales' | 'Acquisition';
+
+// maps to contracts.status
+export type validContractStatusOpts = 'any' | 
+  'Normal' | 'Cancelled' | 'Internal';
+
+// maps to contracts.deal_status
+export type validDealStatusOpts = 'any' | 
+  'Long Form Executed' | 'In Negotiation' | 
+  'Deal Memo Executed' | 'Cancelled' | 'Awaiting Signature';
+
+// titles.status
+export type validProductionStatusOpts = 'any' | 
+  'Completed' | 
+  'Pre-Production' | 'Post-Production' | 'Production' | 
+  'Development' | 'Catalog';
+
+
+// TODO: check column
+// maps to contracts.deal_type ?? 
+export type validDealTypeOpts = 'any' | 
+  'Acquired Content' | 'Representation Only' | 'Produced Content';
+// type validDealTypeOpts = 'Co-Production' | 'Flat Fee' | '[MG] [++MG]' | 'Minimum Guarantee' | 'Output Agrmnt' | 'Production / License Agrmnt' | '[Royalties/Revenue Share]' | 'Straight Distribution' | 'Volume Deal';
+
+
 export interface ContractFilter {
   titleType: {
     operator: validOperators,
-    search: string,
+    search: validTitleTypeOpts,
   },
   title: {
     operator: validOperators,
@@ -19,7 +50,7 @@ export interface ContractFilter {
   },
   contractType: {
     operator: validOperators,
-    search: string,
+    search: validContractTypeOpts,
   },
   contractId: {
     operator: validOperators,
@@ -43,14 +74,18 @@ export interface ContractFilter {
   },
   contractStatus: {
     operator: validOperators,
-    search: string,
+    search: validContractStatusOpts,
   },
   dealStatus: {
     operator: validOperators,
-    search: string,
+    search: validDealStatusOpts,
   },
   productionStatus: {
     operator: validOperators,
-    search: string,
+    search: validProductionStatusOpts,
+  },
+  dealType: {
+    operator: validOperators,
+    search: validDealTypeOpts,
   },
 }
