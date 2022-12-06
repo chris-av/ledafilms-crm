93ca60ccb5d52453833e624017948c0ccc2c8a2f
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-12-06 13:13:41 -0800

    first try at modifying keys

--- pages/api/contracts/filter.ts
+++ pages/api/contracts/filter.ts
@@ -1,6 +1,9 @@
 import Models from '@/db/models';
 const { Contract } = Models;
+import { Op } from 'sequelize';
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { ContractFilter } from '@/utils/interfaces';
+
 
 
 export default async function handler (
@@ -13,9 +16,30 @@ export default async function handler (
       return;
     }
 
-    const filter = req.body;
+    const filter : ContractFilter = req.body;
 
-    res.status(200).json({ message: 'success!', data: req.body });
+    type filterKeys = "titleType" | "title" | "contractType" | "contractId" | "licensor" | "distributor" | "territory" | "right" | "contractStatus" | "dealStatus" | "productionStatus" | "dealType";
+
+    // TODO: how to suppress typescript error?
+    Object.keys(filter).forEach(key => {
+      if (filter[key as filterKeys].search === 'any') {
+        delete filter[key];
+      }
+    });
+
+    // TODO: use utility to transform above into valid sequelize query
+
+    console.log(filter);
+
+    // const result = await Contract.findAll({
+    //   where: {
+    //     contract_id: {
+    //       [Op.iLike]: filter.contractId,
+    //     }
+    //   }
+    // });
+
+    res.status(200).json({ message: 'success!', data: filter });
 
 
   } catch (err) {
