6444ed79d8a1e88468b9df0543ccb3e5692c93c7
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-12-07 18:32:34 -0800

    remove defaultValue prop

--- components/Dropdown.tsx
+++ components/Dropdown.tsx
@@ -21,7 +21,7 @@ export default function Dropdown({ options, callback, value } : ComponentProps) 
     <div className="flex justify-center items-center bg-gray-200 w-full py-1 rounded">
 
       <div className="relative w-full">
-        <select defaultValue={options[0].value} value={value} onChange={handleSelect} className="block appearance-none outline-none w-full bg-gray-200 border border-gray-200 text-gray-700 px-4 rounded leading-tight focus:outline-none">
+        <select value={value} onChange={handleSelect} className="block appearance-none outline-none w-full bg-gray-200 border border-gray-200 text-gray-700 px-4 rounded leading-tight focus:outline-none">
           { options.map(({ label, value }) => (
             <option key={value} value={value}>{label}</option>
           )) }
