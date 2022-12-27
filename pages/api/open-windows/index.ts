b2fc0d707672eb3f8cb33eac045c90432b030d81
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-12-26 22:17:43 -0800

    paginate results

--- pages/api/open-windows/index.ts
+++ pages/api/open-windows/index.ts
@@ -8,7 +8,11 @@ export default async function handler(
   res: NextApiResponse
 ) {
   try {
-    const result = await OpenWindow.findAll();
+    // if no parameters were passed
+    const result = await OpenWindow.findAll({
+      limit: 50,
+      offset: 0,
+    });
     res.status(200).json({ data: result });
   } catch (err: any) {
     res.status(400).json(err);
