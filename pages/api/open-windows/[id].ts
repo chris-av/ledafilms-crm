567fbb5db6160b9f5be9f83321b23ea4baa827f6
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2022-12-26 22:17:37 -0800

    rename query for clarity

--- pages/api/open-windows/[id].ts
+++ pages/api/open-windows/[id].ts
@@ -9,7 +9,6 @@ export default async function handler(
 ) {
   try {
     const { id: contract_id } = req.query;
-    console.log({ contract_id });
     const result = await OpenWindow.findAll({
       where: {
         contract_id
@@ -17,6 +16,7 @@ export default async function handler(
       limit: 50,
       offset: 0,
     });
+    console.log({ contract_id });
     res.status(200).json({ data: result });
   } catch (err: any) {
     res.status(400).json(err);
