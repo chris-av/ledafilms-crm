d76584025b111c3a1efa0ad47cf40a55ee2cd8dd
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-01-01 00:25:03 -0800

    refactor to use context instead of local state

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useThemeContext } from '@/state/theme';
 import Link from 'next/link';
 import SunIcon from '@/components/icons/Sun';
 import MoonIcon from '@/components/icons/Moon';
@@ -7,7 +7,22 @@ import MoonIcon from '@/components/icons/Moon';
 
 export default function Navbar() {
 
-  const [ toggleLight, setToggle ] = useState(false);
+  const { theme, toggleTheme } = useThemeContext();
+
+  const toggleStyle  = {
+    backgroundColor: {
+      light: "#F9FCD1",
+      dark: "black",
+    },
+    circleColor: {
+      light: "#E2BFBF",
+      dark: "white",
+    },
+    position: {
+      light: "translateX(80%)",
+      dark: "translateX(0)",
+    }
+  }
 
   return (
     <div className="w-[300px] h-full bg-slate-100 float-left">
@@ -62,10 +77,10 @@ export default function Navbar() {
       </div>
 
       <div className="cursor-pointer">
-        <label htmlFor="checkbox" onClick={() => setToggle(prev => !prev) } style={{ backgroundColor: toggleLight ? "#F9FCD1" : "black" }} className="relative max-w-[60px] mx-auto bg-[black] flex justify-between items-center px-2 h-[35px] rounded-xl transition-all">
+        <label htmlFor="checkbox" onClick={() => toggleTheme() } style={{ backgroundColor: toggleStyle.backgroundColor[theme as "dark" | "light"] }} className="relative max-w-[60px] mx-auto bg-[black] flex justify-between items-center px-2 h-[35px] rounded-xl transition-all">
           <SunIcon color="#CCD924" size={16} />
           <MoonIcon color="white" size={16} />
-          <div style={{ backgroundColor: toggleLight ? "#E2BFBF" : "white", transform: toggleLight ? "translateX(80%)" : "translateX(0)"  }} className="absolute cursor-pointer w-[24px] h-[24px] rounded-[50%] transition-transform ease-lineari duration-300"></div>
+          <div style={{ backgroundColor: toggleStyle.circleColor[theme as "dark" | "light"], transform: toggleStyle.position[theme as "dark" | "light"] }} className="absolute cursor-pointer w-[24px] h-[24px] rounded-[50%] transition-transform ease-lineari duration-300"></div>
         </label>
       </div>
 
