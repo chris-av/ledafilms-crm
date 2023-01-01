81e25c4f233343f31b5e5ed1593b4531ff752ca5
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-01-01 00:24:05 -0800

    use custom layout

---
+++ design-system/layout/index.tsx
@@ -0,0 +1,28 @@
+import { ReactNode } from 'react';
+import { useThemeContext } from '@/state/theme';
+
+import Navbar from '@/components/Navbar';
+import Topbar from '@/components/Topbar';
+
+interface ComponentProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children } : ComponentProps) {
+  const { theme } = useThemeContext();
+
+  console.log({ theme });
+
+  return (
+    <div className="h-screen w-full overflow-hidden">
+      <Topbar />
+      <div className="flex overflow-hidden h-screen w-screen">
+        <Navbar />
+        <div className="w-full p-8 h-full overflow-auto">
+          { children }
+        </div>
+      </div>
+    </div>
+  );
+
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,17 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import ThemeProvider from '@/state/theme';
+import Layout from '@/design-system/layout';
 import Navbar from '@/components/Navbar';
 import Topbar from '@/components/Topbar';
 import "@/styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="h-screen w-full overflow-hidden">
-      <Topbar />
-      <div className="flex overflow-hidden h-screen w-screen">
-        <Navbar />
-        <div className="w-full p-8 h-full overflow-auto">
-          <Component {...pageProps} />
-        </div>
-      </div>
-    </div>
+    <ThemeProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   );
 }
 
