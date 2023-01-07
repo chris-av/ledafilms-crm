75ed363d4b4f55532861836fb995cb5787cea302
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-01-07 13:48:30 -0800

    make styling optional

--- components/icons/Plus.tsx
+++ components/icons/Plus.tsx
@@ -1,9 +1,9 @@
 interface ComponentProps {
-  size: number,
-  color: string,
+  size?: number,
+  color?: string,
 }
 
-export default function Plus({ size, color = "currentColor" } : ComponentProps) {
+export default function Plus({ size = 16, color = "currentColor" } : ComponentProps) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} fill={color} className="bi bi-plus-lg" viewBox="0 0 16 16">
       <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2Z"/>
