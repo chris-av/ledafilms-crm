4cdc2b1017985780625b42454a710570804dc3af
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-01-09 16:59:58 -0800

    remove unused imports

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,6 @@
 import type { AppProps } from 'next/app';
 import ThemeProvider from '@/state/theme';
 import Layout from '@/design-system/layout';
-import Navbar from '@/components/Navbar';
-import Topbar from '@/components/Topbar';
 import "@/styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
