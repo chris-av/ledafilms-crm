221dbc61ad741926d7c969379cef5f30c1cebf64
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-01-09 17:43:48 -0800

    add expand/contract icon

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,13 +1,16 @@
+import classNames from 'classnames';
 import { useThemeContext } from '@/state/theme';
+import { useNavContext } from '@/state/navopen';
 import Link from 'next/link';
 import SunIcon from '@/components/icons/Sun';
 import MoonIcon from '@/components/icons/Moon';
-
+import ArrowOut from '@/components/icons/ArrowBarLeft';
 
 
 export default function Navbar() {
 
   const { theme, toggleTheme } = useThemeContext();
+  const { open, toggleOpen } = useNavContext();
 
   const toggleStyle  = {
     backgroundColor: {
@@ -25,9 +28,24 @@ export default function Navbar() {
   }
 
   return (
-    <div className="w-[300px] h-full bg-slate-100 float-left">
+    <div className={classNames("max-w-[300px] h-full bg-slate-100 float-left transition-all ease-in-out", {
+      "w-full": open === true,
+      "w-[80px]": open === false,
+      })}>
 
-      <div className="flex flex-col h-[85%]">
+      <div className="w-full flex justify-start my-8 relative">
+        <button className={classNames("absolute left-6 text-center transition-transform origin-center", {
+          "rotate-0": open == true,
+          "rotate-180": open == false,
+          })} onClick={toggleOpen}>
+          <ArrowOut size={26} />
+        </button>
+      </div>
+
+      <div className={classNames("flex flex-col h-[85%]", {
+        "flex": open === true,
+        "invisible": open === false,
+        })}>
         <ul className="w-full flex flex-col items-center text-xl h-[50%]">
 
           <li className="flex items-center w-full h-[60px] pl-8">
