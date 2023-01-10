184b47e876cc1be7153b28efc64a498748d20413
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-01-09 23:45:22 -0800

    restructure intial state for filter

--- data/filter-state.data.ts
+++ data/filter-state.data.ts
@@ -1,52 +1,15 @@
-import { ContractFilter } from '@/utils/interfaces';
-
-export const filterState : ContractFilter = {
-  titleType: {
-    operator: '=',
-    search: 'any',
-  },
-  title: {
-    operator: '~',
-    search: '',
-  },
-  contractType: {
-    operator: '=',
-    search: 'any',
-  },
-  contractId: {
-    operator: '~',
-    search: '',
-  },
-  licensor: {
-    operator: '~',
-    search: '',
-  },
-  distributor: {
-    operator: '~',
-    search: '',
-  },
-  territory: {
-    operator: '~',
-    search: '',
-  },
-  right: {
-    operator: '~',
-    search: '',
-  },
-  contractStatus: {
-    operator: '=',
-    search: 'any',
-  },
-  dealStatus: {
-    operator: '=',
-    search: 'any',
-  },
-  productionStatus: {
-    operator: '=',
-    search: 'any',
-  },
-  dealType: {
-    operator: '=',
-    search: 'any'
-  }
-}
+export const initialFilterState = {
+  titleType: "",
+  title: "",
+  contractType: "",
+  contractId: "",
+  licensor: "",
+  distributor: "",
+  territory: "",
+  right: "",
+  contractStatus: "",
+  dealStatus: "",
+  productionStatus: "",
+  dealType: "",
+  project_code: "",
+};
