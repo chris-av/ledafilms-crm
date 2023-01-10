9f28bf7e6b9b5d8d157ad5a895fb92af6552525d
Christian Valenzuela <41182258+chris-av@users.noreply.github.com>
2023-01-09 18:20:23 -0800

    move the consts to index.tsx

--- utils/interfaces/contract.ts
+++ utils/interfaces/contract.ts
@@ -1,16 +1,3 @@
-export const ContractTypes = ["Sales", "Acquistion"] as const;
-export const ContractStatuses = ["Normal", "Internal", "Cancelled"] as const;
-export const DealStatuses = ["Awaiting Signature", "Cancelled", "Deal Memo Executed", "In Negotiation", "Long Form Executed"] as const;
-
-export const ProductionStatuses = ["Completed", "Post-Production", "Production", "Development", "Catalog", "Pre-Production"] as const;
-
-export const DealTypes = ["[MG] [++Mg]", "[Royalties/Revenue Share]", 
-  "Co-Production", "Flat Fee", "Minimum Guarantee", "Output Agent", "Production / License Agrmnt",
-  "Straight Distribution", "Volume Deal"] as const;
-
-
-
-
 export interface IContract {
   contract_type: string,
   licensor: string,
@@ -29,5 +16,4 @@ export interface IContract {
 
 
 export interface IContracts extends Array<IContract>{};
-
 
--- utils/interfaces/index.ts
+++ utils/interfaces/index.ts
@@ -1,3 +1,15 @@
+export const ContractTypes = ["Sales", "Acquistion"] as const;
+export const ContractStatuses = ["Normal", "Internal", "Cancelled"] as const;
+export const DealStatuses = ["Awaiting Signature", "Cancelled", "Deal Memo Executed", "In Negotiation", "Long Form Executed"] as const;
+
+export const ProductionStatuses = ["Completed", "Post-Production", "Production", "Development", "Catalog", "Pre-Production"] as const;
+
+export const DealTypes = ["[MG] [++Mg]", "[Royalties/Revenue Share]", 
+  "Co-Production", "Flat Fee", "Minimum Guarantee", "Output Agent", "Production / License Agrmnt",
+  "Straight Distribution", "Volume Deal"] as const;
+
+
+
 export * from './contract';
 export * from './talent';
 export * from './open-window';
